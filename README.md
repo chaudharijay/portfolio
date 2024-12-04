99cd7c5bdb1039584851fb460aa1e206bef83189
Yash Chaudhari <114210713+chaudharijay@users.noreply.github.com>
2024-12-04 23:21:47 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,102 @@
+# Web Portfolio Readme
+
+This repository contains the code and assets for my personal web portfolio, showcasing my projects, skills, and experience. The portfolio is built using HTML, CSS, JavaScript, and React. It's designed to provide an interactive and visually appealing way to display my work and achievements.
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Features](#features)
+- [Technologies Used](#technologies-used)
+- [Getting Started](#getting-started)
+  - [Installation](#installation)
+  - [Running the Portfolio](#running-the-portfolio)
+- [Customization](#customization)
+- [Deployment](#deployment)
+- [License](#license)
+
+## Introduction
+
+Welcome to my personal web portfolio repository! This portfolio website serves as a showcase of my projects, skills, and professional experience. It's designed to leave a lasting impression on visitors and potential collaborators. The portfolio is built using modern web technologies and follows best practices for responsiveness, accessibility, and user experience.
+
+![Screenshot (158)](https://github.com/RSN601KRI/portfolio/assets/106860359/660f7832-1ca8-44a9-83ae-c4481b38f561)
+
+
+## Features
+
+- **Home Page:** A brief introduction about me along with links to my social media profiles.
+- **Projects:** Display of my projects with detailed descriptions, images, and links to GitHub repositories or live demos.
+- **Skills:** List of my technical skills and competencies.
+- **Resume:** A downloadable PDF version of my resume/CV.
+- **Contact:** A contact form for visitors to get in touch with me directly.
+- **Responsive Design:** The portfolio is optimized for various screen sizes, including mobile devices and tablets.
+
+## Technologies Used
+
+- HTML5: Structuring the web content.
+- CSS3: Styling and layout design.
+- JavaScript: Adding interactivity to the portfolio.
+- React: Building the portfolio as a dynamic single-page application.
+- [React Router](https://reactrouter.com/): Handling navigation within the portfolio.
+- [EmailJS](https://www.emailjs.com/): Enabling the contact form functionality.
+- Version Control: Git and GitHub for tracking changes and collaboration.
+
+## Getting Started
+
+Follow these steps to set up the portfolio locally on your machine:
+
+### Installation
+
+1. Clone this repository to your local machine using:
+
+   ```bash
+   git clone https://github.com/your-username/your-portfolio.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd your-portfolio
+   ```
+
+3. Install the project dependencies:
+
+   ```bash
+   npm install
+   ```
+
+### Running the Portfolio
+
+1. Start the development server:
+
+   ```bash
+   npm start
+   ```
+
+2. Open your web browser and visit `http://localhost:3000` to see your portfolio.
+
+## Customization
+
+Feel free to customize the portfolio to make it your own:
+
+- Replace the placeholder content with your own information in the `src/data` directory.
+- Modify the styling by editing the CSS files in the `src/styles` directory.
+- Add your own projects by creating new components in the `src/components` directory.
+
+## Deployment
+
+To deploy your portfolio online, consider using platforms like:
+
+- [Netlify](https://www.netlify.com/)
+- [Vercel](https://vercel.com/)
+- [GitHub Pages](https://pages.github.com/)
+
+Remember to update the deployment settings to reflect your chosen platform and configure any necessary build scripts.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+---
+
+Thank you for checking out my web portfolio! If you have any questions or suggestions, feel free to get in touch with me.
+If you like please marked a Star⭐
---
+++ src/data/constants.js
@@ -0,0 +1,427 @@
+export const Bio = {
+  name: "Yash Chaudhari",
+  roles: [
+    "Data Scientist",
+    "Full Stack Developer",
+    "AI/ML Engineer",
+    "Data Analyst",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/chaudharijay",
+  resume:
+    "https://drive.google.com/file/d/1nS62JRfBLOhrSfy2pNmvUqrMmwZVXK-M/view?usp=sharing",
+  linkedin: "www.linkedin.com/in/yash-chaudhari-a100451abx",
+  twitter: "",
+  insta: "",
+  facebook: "",
+};
+
+export const skills = [
+  {
+    title: "MERN Stack",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://www.vectorlogo.zone/logos/expressjs/expressjs-icon.svg",
+      },
+      {
+        name: "Mongo Db",
+        image:"https://www.vectorlogo.zone/logos/mongodb/mongodb-icon.svg"
+      }
+    ],
+  },
+  {
+    title: "Data Science",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "R",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/1/1b/R_logo.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://www.vectorlogo.zone/logos/mysql/mysql-official.svg",
+      },
+      {
+        name: "Statistics",
+        image:"https://img.icons8.com/?size=100&id=mOCU2P4rcj4p&format=png&color=000000"
+      },
+      {
+        name: "Machine Learning",
+        image:"https://upload.wikimedia.org/wikipedia/commons/d/d5/Hey_Machine_Learning_Logo.png"
+      },
+      {
+        name: "AI",
+        image:"https://upload.wikimedia.org/wikipedia/commons/0/04/ChatGPT_logo.svg"
+      },
+     
+    ],
+  },
+  {
+    title: "Libraries",
+    skills: [
+      {
+        name: "Pandas",
+        image:"https://upload.wikimedia.org/wikipedia/commons/e/ed/Pandas_logo.svg"
+      },
+      {
+        name: "NumPy",
+        image:"https://upload.wikimedia.org/wikipedia/commons/3/31/NumPy_logo_2020.svg"
+      },
+      {
+        name: "SciPy",
+        image:"https://upload.wikimedia.org/wikipedia/commons/b/b2/SCIPY_2.svg"
+      },
+      {
+        name: "Matplotlib",
+        image:"https://upload.wikimedia.org/wikipedia/commons/8/84/Matplotlib_icon.svg"
+      },
+      {
+        name: "Seaborn",
+        image:"https://seaborn.pydata.org/_images/logo-tall-lightbg.svg"
+      },
+      {
+        name: "Scikit-learn",
+        image:"https://upload.wikimedia.org/wikipedia/commons/0/05/Scikit_learn_logo_small.svg"
+      },
+      {
+        name:"Keras",
+        image:"https://upload.wikimedia.org/wikipedia/commons/a/ae/Keras_logo.svg"
+      },
+     
+     
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://img.icons8.com/?size=100&id=20906&format=png&color=000000",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "PyCharm",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/1/1d/PyCharm_Icon.svg",
+      },
+      {
+        name: "Tensorflow",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/a/ab/TensorFlow_logo.svg",
+      },
+    ],
+  },
+];
+
+export const certifications = [
+
+  {
+    id: 0,
+    company: "Google Cloud Platform (GCP)",
+    img: "https://gwcteq.com/wp-content/uploads/2023/05/Emblem-Google-Cloud.jpg",
+    date: "July 2023 - july 2025",
+    desc: "GCP is a platform where wide range of online courses are for free to learn with hands on lab experience and challenges to do which boosts our skills in various Cloud Technologies. I've done some of the certifications from GCP.",
+    tag: "Google Cloud Platform",
+    doc: "https://drive.google.com/file/d/1w6nLh0aXnrvI6x6XGM9uz1yGMCd5HPiQ/view?usp=sharing",
+  },
+  
+  {
+    id: 0,
+    company: "MERN Stack Developer By GFG",
+    img: "https://upload.wikimedia.org/wikipedia/commons/4/43/GeeksforGeeks.svg",
+    date: "Jan 2024 - Jan 2025",
+    desc: "GeeksforGeeks is a leading platform that provides computer science resources and coding challenges for programmers and technology enthusiasts, along with interview and exam preparations for upcoming aspirants. With a strong emphasis on enhancing coding skills and knowledge, it has become a trusted destination for over 12 million plus registered users worldwide. The platform offers a vast collection of tutorials, practice problems, interview tutorials, articles, and courses, covering various domains of computer science.",
+    tag: "GFG",
+    doc: "https://drive.google.com/file/d/1HBraSeMztj-ZUcE5VUQvfuBnh4zP04pm/view?usp=sharing",
+  },
+  {
+    id: 0,
+    company: "Guvi",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQXyYKvvMg_bFcAlOn3gNaFTEffaLASj0WmA&s",
+    date: "August 2023 - March 2025",
+    desc: "GUVI (Grab Your Vernacular Imprint) is an online learning platform that offers coding courses and other technical skills training in multiple languages.",
+    tag: "Python Language Completion",
+    doc: "https://drive.google.com/file/d/11HRVH9FQ4Rr0STXq999-Nh9Fi0cxKBNw/view?usp=sharing",
+  },
+  {
+    id: 0,
+    company: "HackerRank",
+    img: "https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/160_Hackerrank_logo_logos-512.png",
+    date: "July 2023 - july 2025",
+    desc: "HackerRank is a platform where people can share their skills. I've done some of the certifications from HackerRank.",
+    tag: "HackerRank Certification",
+    doc: "https://www.hackerrank.com/certificates/iframe/d1f4ee51eb86",
+  },
+  {
+    id: 0,
+    company: "TCS ESG Virtual Experience Program",
+    img: "https://companieslogo.com/img/orig/TCS.NS-7401f1bd.png?t=1720244494",
+    date: "August 2023 - Jan 2025",
+    desc: "Launched in 2017, Forage invented the job simulation. We give any student, from any diverse starting point in the world, the chance to see behind the curtains of the best companies around the world and start careers there–100% free & self-paced.",
+    tag: "Forage",
+    doc: "https://drive.google.com/file/d/1OF59MNZktA-gOljphE8WqIL6Vwz7CJWy/view?usp=sharing",
+  },
+ 
+ 
+  {
+    id: 0,
+    company: "Tata Data Visualization",
+    img: "https://upload.wikimedia.org/wikipedia/commons/8/8e/Tata_logo.svg",
+    date: "August 2023 - Jan 2025",
+    desc: "Launched in 2017, Forage invented the job simulation. We give any student, from any diverse starting point in the world, the chance to see behind the curtains of the best companies around the world and start careers there–100% free & self-paced.",
+    tag: "Forage",
+    doc: "https://drive.google.com/file/d/1GWR2A1H7jyG3czfgE1xKbDN806vGF4kZ/view?usp=sharing",
+  }
+]
+export const experiences = [
+  {
+    id: 0,
+    img: "",
+    role: "Data Scientist",
+    company: "Mahajan Industries pvt. ltd.",
+    date: "Nov 2023 - Dec 2023",
+    desc: " Worked with data scientists to analyze sales data by part and identify the most loyal customers.",
+    skills: [
+      "Python",
+      "MySQL",
+      "Pandas",
+      "Matplotlib",
+      "SciPy",
+      "Sklearn",
+    ],
+    doc: "", 
+  }
+];
+
+export const education = [
+  {
+    id: 0,
+    school: "SSVPS's B.S. Deore College of Engineering, Dhule",
+    date: "2020 - 2024",
+    grade: "8.01 CGPA",
+    desc: "I have finished pursuing my Bachelor's degree in Computer Science and Engineering at SSVPS College of Engineering, Dhule. I have done courses such as MERN Stack and Data Science with AI/ML engineer. And currently specializing in Data Science.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    school: "Jai Hind Highschool, Dhule",
+    date: "2019 - 2020",
+    grade: "70%",
+    desc: "I completed my class 12 high school education at Jai Hind Highschool, Dhule, where I studied Science with Information Technology.",
+    degree: "HSC(XII), Science with I.T.",
+  },
+  {
+    id: 2,
+    school: "Yashwant Vinayan Mandir, Dhule",
+    date: "2017-2018",
+    grade: "79.80%",
+    desc: "I completed my class 10 education at Yashwant Vinayan Mandir, Dhule, where I studied Science with Computer Application.",
+    degree: "SSC(X), Science with Computer",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Quora",
+    date: "Jun 2024 - Jul 2024",
+    description:
+      "Kaggle competition project where grouping simiilar questions can solve heavy computation and provide more accurate answers.",
+    image:
+      "",
+    tags: [
+      "Python",
+      "Streamlit",
+      "XgBoost",
+      "NLP",
+      "CV",
+      "Pickle",
+      "Fuzzywuzzy",
+      
+    ],
+    category: "web app",
+    github: "https://github.com/chaudharijay/Quora.git",
+    webapp: "https://quora-similar-ques.streamlit.app",
+  },
+
+  {
+    id: 1,
+    title: "T20",
+    date: "Oct 2024 - Nov 2024",
+    description:
+      "Using Machine Learning T20 can predict the performance(score) of a cricket team for a respective match may be past or future. ",
+    image:
+      "",
+    tags: [
+      "Python",
+      "Machine Learning",
+      "Streamlit",
+      "Random Forest",
+      "YAML",
+    ],
+    category: "web app",
+    github: "https://github.com/chaudharijay/T20-Score-Predict",
+    webapp: "https://chaudharijay-t20-score-predict-app-k5rhq8.streamlit.app",
+  },
+  {
+    id: 9,
+    title: "Youtuber's Info",
+    date: "Nov 2024 - Dec 2024",
+    description:
+      "Using Youtubes API this notebook gives user all the information about their favorite youtuber's channel. Just replace youtube channel ID",
+    image:
+      "",
+    tags: [
+      "Python",
+      "Pandas",
+      "API",
+      "seaborn",
+      "matplotlib",
+      "nltk",
+      "wordcloud",
+      
+    ],
+    category: "web app",
+    github: "https://github.com/chaudharijay/youtube-stats-using-API-.git",
+    webapp: "",
+  },
+  {
+    id: 0,
+    title: "Placement Prediction",
+    date: "Jan 2024 - May 2024",
+    description:
+      "Placement Prediction is a WebApp where students can predict their chances of getting placed with new criterias in the market.",
+    image:
+      "",
+    tags: ["React Js", "MongoDb", "Node Js", "Express Js", "AI/ML", "Data Science"],
+    category: "android app",
+    github: "https://github.com/chaudharijay/Campus-Placement-Predictor-Webapp-Using-ML.git",
+    webapp: "",
+  },
+ 
+  {
+    id: 3,
+    title: "Tech Titans: Exploring the Top Trending Technologies of Tomorrow",
+    date: "Apr21, '23",
+    description:
+      "Exploring the top tech trends shaping the future of our World.",
+    image:
+      "https://images.unsplash.com/photo-1605810230434-7631ac76ec81?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    tags: ["technology","ai/ml","blockchain"],
+    category: "articles",
+    link: "",
+    read: "https://blogeroshniii.hashnode.dev/tech-titans-exploring-the-top-trending-technologies-of-tomorrow",
+  },
+  {
+    id: 10,
+    title: "Protecting Your Business: How to Safeguard Against Data Breaches and Cyber Attacks🛡🌐",
+    date: "Jun 18, '23",
+    description:
+      "As a business owner, one of the most critical aspects of running a successful company is safeguarding your data against potential breaches and cyber attacks. In today's digital age, data breaches have become increasingly common, and the consequences can be devastating for businesses of all sizes. In this article, we will explore the various types of data breaches and cyber attacks, understand their impact on businesses, and discuss best practices for protecting your valuable information.",
+    image:
+      "https://images.unsplash.com/photo-1550751827-4bd374c3f58b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    tags: ["tech", "cyber security", "blockchain"],
+    category: "articles",
+    link: "",
+    read: "https://blogeroshniii.hashnode.dev/protecting-your-business-how-to-safeguard-against-data-breaches-and-cyber-attacks",
+  },
+  {
+    id: 4,
+    title: "AWS Amplify Getting Started Guide🚀",
+    date: "Aug 24, '23",
+    description:
+      "AWS Amplify is a set of tools and services that enables developers to build full-stack web and mobile applications with ease. It provides features like authentication, API integration, storage, and more, simplifying the development process and reducing the time to market your applications. This guide will walk you through the steps to install and configure AWS Amplify, create a sample project, use the Amplify Editor, and deploy the project.",
+    image:
+      "https://images.unsplash.com/photo-1607041409099-bd92431680a1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    tags: ["aws web", "Amplification", "AWS", "Lambda"],
+    category: "articles",
+    link: "",
+    read: "https://blogeroshniii.hashnode.dev/aws-amplify-getting-started-guide",
+  },
+  {
+    id: 7,
+    title: "Cracking the Code: How to Make Money Online in a Competitive Future🚀",
+    date: "Aug 10, '23",
+    description:
+      "In today’s digital age, earning money online has become a popular option for people who want to work from home and have the flexibility to choose their own hours. However, with the rise of technology and the internet, competition in the online marketplace is increasing rapidly.This blog will outline how to earn money online through skills and learning while still facing high competition in the future. We will discuss various strategies and techniques that you can use to stand out from the crowd and succeed in the online world.🌍",
+    image:
+      "https://images.unsplash.com/photo-1499909694555-1ae5b7067b1a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    tags: ["skills", "management"],
+    category: "articles",
+    link: "",
+    read: "https://medium.com/@roshni_k06/cracking-the-code-how-to-make-money-online-in-a-competitive-future-d6276a510a81",
+  },
+  {
+    id: 8,
+    title: "Unlocking the Power of Microsoft Azure: A Beginner's Guide 🌐",
+    date: "Feb 15, '24",
+    description:
+      "In today's digital age, harnessing the potential of cloud computing has become imperative for businesses striving for innovation and efficiency. Microsoft Azure stands out as a comprehensive cloud platform offering a myriad of services tailored to meet diverse needs. Let's embark on a journey to explore the fundamentals of Microsoft Azure, demystifying its capabilities and empowering you to leverage its full potential!",
+    image:
+      "https://images.unsplash.com/photo-1561736778-92e52a7769ef?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    tags: ["Azure", "Cloud Computing", "Excel", "AI/ML"],
+    category: "articles",
+    link: "",
+    read: "https://blogeroshniii.hashnode.dev/unlocking-power-of-microsoft-azure-a-beginners-guide",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
